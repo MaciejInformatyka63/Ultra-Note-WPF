10c7259fceec86b1a317398d1dcb17ebc0ececed
mabizet <mabizet@05e6fd3b-8aa4-46d5-bce3-c2b831b5dbcb>
2021-04-02 13:25:17 +0000

    création de la vue sur le mode gestionnaire

---
+++ Source/Vue Application/Vue Application/ModeGestionnaire.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vue_Application
+{
+    /// <summary>
+    /// Logique d'interaction pour ModeGestionnaire.xaml
+    /// </summary>
+    public partial class ModeGestionnaire : Window
+    {
+        public ModeGestionnaire()
+        {
+            InitializeComponent();
+        }
+    }
+}
