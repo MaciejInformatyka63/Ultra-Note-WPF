f83c3ad010580404024d01bedb130045bb197481
mabizet <mabizet@05e6fd3b-8aa4-46d5-bce3-c2b831b5dbcb>
2021-05-17 06:38:58 +0000

    j'ai renommé les tests unitaires en TestUnit[classe] et supprimé celui sur la classe Utilisateur

---
+++ Source/Vue Application/Tests_Unitaires/TestUnitNote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using Modele;
+
+namespace Tests_Unitaires
+{
+    public class TestUnitNote
+    {
+        [Fact]
+        public void CréationNote()
+        {
+            Note carré = new Note("carré", "Un carré est un rectangle particulier");
+        }
+
+        [Fact]
+        public void EditionCommentaire()
+        {
+            Commentaire com = new Commentaire("ceci est un commentaire");
+        }
+    }
+}
