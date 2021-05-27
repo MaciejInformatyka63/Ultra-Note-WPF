7f14eebba11594d41847c8b697fb46e7d7c7b091
thdevienne <thdevienne@05e6fd3b-8aa4-46d5-bce3-c2b831b5dbcb>
2021-05-27 16:38:16 +0000

    Binding sur la RichTextBox (affichage du contenu de la note mais les modifications ne sont pas sauvées). Temporairement, pour visualiser le contenu d'une note (le balisage XML) et pouvoir le modifier, il faut décommenter les lignes 325 et 326 dans MainWindow.xaml puis éditer le contenu de la note directement dans la TextBox.

--- Source/UltraNotes/UltraNotes/VueModele/FlowDocumentToXamlConverter.cs
+++ Source/UltraNotes/UltraNotes/VueModele/FlowDocumentToXamlConverter.cs
@@ -23,7 +23,7 @@ namespace UltraNotes.VueModele
             if (value != null)
             {
                 var xamlText = (string)value;
-                flowDocument = (FlowDocument)XamlReader.Parse(xamlText as string);
+                flowDocument = (FlowDocument)XamlReader.Parse(xamlText);
             }
 
             // Set return value
