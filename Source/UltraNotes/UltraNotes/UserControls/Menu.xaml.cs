7db3703774a5ccfac2c838ea5c21e2c68cd6ee14
mabizet <mabizet@05e6fd3b-8aa4-46d5-bce3-c2b831b5dbcb>
2021-05-30 13:10:20 +0000

    ajout d'unUC pour la barre de menu (j'ai laissé l'ancien menu en commentaire dans MainWindow.xaml) remplissage des polices et de leur taille dans une méthode Initialize() dans le code behind début de binding ( menuItem "Undo" )

---
+++ Source/UltraNotes/UltraNotes/UserControls/Menu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UltraNotes.UserControls
+{
+    /// <summary>
+    /// Logique d'interaction pour Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            this.Initialize();
+        }
+        private void Initialize()
+        {
+            FontFamilyMenu.ItemsSource = Fonts.SystemFontFamilies;
+            FontSizeMenu.Items.Add("10");
+            FontSizeMenu.Items.Add("12");
+            FontSizeMenu.Items.Add("14");
+            FontSizeMenu.Items.Add("16");
+            FontSizeMenu.Items.Add("18");
+            FontSizeMenu.Items.Add("24");
+            FontSizeMenu.Items.Add("32");
+        }
+    }
+}
